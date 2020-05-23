f3cd0654e3f504c92ee3ed8998c671a35cf1fd51
Andres Torres <jatobrun@espol.edu.ec>
2020-05-23 11:14:23 -0500

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=admin123
+export POSTGRES_PASSWORD=admin123
+export POSTGRES_HOST=database-1.cgtj4vydjmas.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-105052097776-prod
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=hola
+export URL=http://localhost:8100
